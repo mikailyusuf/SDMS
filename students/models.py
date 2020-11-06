94b0bf46f868502a11f45ad6f01e5aba0b588425
Mikail Yusuf <mikailkyusuf@gmail.com>
2020-11-06 07:50:31 +0100

    Added some Models

--- students/models.py
+++ students/models.py
@@ -1,3 +1,66 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Students(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    first_name = models.CharField(max_length=200, null=False)
+    last_name = models.CharField(max_length=200, null=False)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    profile_pic = models.ImageField(null=True,blank=True)
+
+    def __str__(self):
+        return self.email
+
+
+class Teachers(models.Model):
+    first_name = models.CharField(max_length=200, null=False)
+    last_name = models.CharField(max_length=200, null=False)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    profile_pic = models.ImageField(null=True,blank=True)
+
+    def __str__(self):
+        return self.email
+
+
+class Result(models.Model):
+    student = models.ForeignKey(Students, null=False, on_delete=models.SET_NULL)
+
+    math_exan = models.FloatField(null=True)
+    math_test = models.FloatField(null=True)
+    math_total = models.FloatField(null=True)
+
+    eng_exan = models.FloatField(null=True)
+    eng_test = models.FloatField(null=True)
+    eng_total = models.FloatField(null=True)
+
+
+    physics_exan = models.FloatField(null=True)
+    physics_test = models.FloatField(null=True)
+    physics_total = models.FloatField(null=True)
+
+    bio_exan = models.FloatField(null=True)
+    bio_test = models.FloatField(null=True)
+    bio_total = models.FloatField(null=True)
+
+    chem_exan = models.FloatField(null=True)
+    chem_test = models.FloatField(null=True)
+    chem_total = models.FloatField(null=True)
+
+    agric_exan = models.FloatField(null=True)
+    agric_test = models.FloatField(null=True)
+    agric_total = models.FloatField(null=True)
+
+    civic_exan = models.FloatField(null=True)
+    civic_test = models.FloatField(null=True)
+    civic_total = models.FloatField(null=True)
+
+
+    comment = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
--- students/views.py
+++ students/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.models import Group
 from django.shortcuts import render
 
 
 # Create your views here.
+from students.forms import CreateUserForm
+from students.models import Teachers
+
 
 def login(request):
     return render(request, 'students/login.html')
@@ -12,3 +16,17 @@ def signup(request):
 
 def home(request):
     return render(request, 'students/home.html')
+
+def createUser(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = createUser(request)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            group = Group.objects.get(name='teachers')
+            user.groups.add(group)
+        Teachers.objects.create(
+        user = user,first_name =
+        )
